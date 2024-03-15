5dd95560b4e2941b117a7b7d7dd410235e33aa7b
TusharD <tushar937297@gmail.com>
2024-03-15 23:42:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Todo from './components/Todo'
+import AddTodo from './components/AddTodo'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Learn about Redux toolkit</h1>
+     <AddTodo />
+     <Todo />
+    </>
+  )
+}
+
+export default App
